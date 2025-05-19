065650a7b0ca90d52ea8057fd32fbb3b5d377dc6
Yuriy Chebyshev <bomzheg@gmail.com>
2025-05-19 21:40:29 +0300

    added few more analytics

--- cylinders/analytics/diff_cylinders_count.py
+++ cylinders/analytics/diff_cylinders_count.py
@@ -1,5 +1,6 @@
-from calendar import Calendar, monthrange
+from calendar import Calendar, calendar, monthrange
 from datetime import date
+from decimal import Decimal
 
 import psycopg2.extras
 from psycopg2.extensions import cursor
@@ -14,8 +15,9 @@ def cylinders_day_count(cur: cursor, day_from: date, day_to: date):
             SELECT 
                 so."Колличество в связке", 
                 so."Объём баллона, л", 
-                so."Давление, ат",  
-                SUM(bp."Количество")
+                so."Давление, ат",
+                so."Объём газа, м3",
+                SUM(bp."Количество") as sum
             FROM "Баллоны в партиях" bp 
             INNER JOIN "Партии" p
                 ON p.id = bp."ID партии"
@@ -35,8 +37,8 @@ def get_count_cylinders_by_weaks(month: int, year: int):
             text_result += f"\nНа конец недели {sunday}:\n"
             week_total_cylinders = 0
             for cylinder in cur:
-                if cylinder['Количество в связке'] > 1:
-                    monoblock_modifier = f"{cylinder['Количество в связке']}*"
+                if cylinder['Колличество в связке'] > 1:
+                    monoblock_modifier = f"{cylinder['Колличество в связке']}*"
                 else:
                     monoblock_modifier = ""
                 text_result += (
@@ -48,7 +50,7 @@ def get_count_cylinders_by_weaks(month: int, year: int):
         return text_result
 
 
-def get_count_cylinders(month: int, year: int):
+def get_count_cylinders(month: int, year: int) -> tuple[str, int, Decimal]:
     with connect_pg(config.db_config) as conn, conn.cursor(cursor_factory=psycopg2.extras.DictCursor) as cur:
         text_result = f"Отгружено баллонов в {month}.{year}:"
         _, last_month_day = monthrange(year, month)
@@ -56,20 +58,27 @@ def get_count_cylinders(month: int, year: int):
         end_date = date(year, month, last_month_day)
         cylinders_day_count(cur, start_date, end_date)
         text_result += f"\n{year}-{month}:\n"
+        total_gaz = Decimal(0)
         total_cylinders = 0
         for cylinder in cur:
-            if cylinder['Количество в связке'] > 1:
-                monoblock_modifier = f"{cylinder['Количество в связке']}*"
+            if cylinder['Колличество в связке'] > 1:
+                monoblock_modifier = f"{cylinder['Колличество в связке']}*"
             else:
                 monoblock_modifier = ""
             text_result += (
-                f"{monoblock_modifier}{cylinder['Объём баллона, л']} л "
-                f"({cylinder['Давление, ат']} ат.): {cylinder['sum']}\n"
+                f"{monoblock_modifier}{cylinder['Объём баллона, л']} "
+                f"{cylinder['Давление, ат']} {cylinder['sum']}\n"
             )
             total_cylinders += cylinder['sum']
-        text_result += f"Итого за месяц баллонов: {total_cylinders}\n"
-        return text_result
+            total_gaz += cylinder["Объём газа, м3"] * cylinder["sum"]
+        text_result += f"Итого за месяц баллонов: {total_cylinders}, газа: {total_gaz} м3\n"
+        return text_result, total_gaz, total_cylinders
 
 if __name__ == "__main__":
+    total_gaz = Decimal(0)
+    total_cylinders = 0
     for i in range(1, 12):
-        print(get_count_cylinders(i, 2024))
+        _, gaz, cylinders = get_count_cylinders(i, 2024)
+        total_gaz += gaz
+        total_cylinders += cylinders
+    print(f"{total_gaz} - {total_cylinders}")
